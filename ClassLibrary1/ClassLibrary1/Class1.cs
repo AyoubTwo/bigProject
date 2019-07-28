8edeb0bc9d7a42182cac0af1dd475ccfbd277eab
unknown <ayoub.chaabi01@gmail.com>
2019-07-28 16:21:49 +0000

    [Test] Add a change to devbranch

--- ClassLibrary1/ClassLibrary1/Class1.cs
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -4,5 +4,9 @@ namespace ClassLibrary1
 {
     public class Class1
     {
+        public void ShowUser()
+        {
+            Console.Write("this is ayoubtwo source code ! ");
+        }
     }
 }
